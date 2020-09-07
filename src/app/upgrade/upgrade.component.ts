4290b9e15a9615f350e895ca32e6e1406dcbe2cc
Timofey Kasimtsev <s2tikas@uni-trier.de>
2020-09-07 21:47:03 +0200

    Dynamic initialization of Databases and APIs

--- src/app/upgrade/upgrade.component.ts
+++ src/app/upgrade/upgrade.component.ts
@@ -14,18 +14,36 @@ export class UpgradeComponent implements OnInit {
 
   config = null;
   serverError = null;
-  metrics;
+  metrics = null;
+  dbs = null;
+  apis = null;
+  databases = null;
 
   constructor( private http: HttpClient) {
     console.log('test');
-    this.getConfigs(); }
+    this.getConfigs();
+    this.getDatabasesAndAPIs();
+  }
 
   private getConfigs () {
     this.http.get(environment.configURL).subscribe(data => {
       this.config = JSON.parse(JSON.stringify(data));
-      this.metrics = data.linkage_config.similarity_metrics;
+     this.metrics = data.linkage_config.similarity_metrics;
      // console.log(data.linkage_config.similarity_metrics);
      // console.log(this.metrics[1]);
+    }, error => {
+      this.serverError = true;
+    });
+
+
+  }
+
+  private getDatabasesAndAPIs () {
+
+    this.http.get(environment.databaseURL).subscribe(data1 => {
+      this.databases = JSON.parse(JSON.stringify(data1));
+      this.dbs = data1.endpoints;
+       this.apis = data1.apis;
     }, error => {
       this.serverError = true;
     });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ const base_url = 'http://localhost:7070/';
 
 export const environment = {
   production: false,
-  configURL : base_url + 'config'
+  configURL : base_url + 'config',
+  databaseURL: base_url + 'databases'
 };
